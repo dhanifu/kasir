e9f33b3e4ee1b940a16adc8c39e25a4b7414e563
dhanifu <mramdhani010@gmail.com>
2021-01-14 23:28:21 +0700

    fix typo

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -2,29 +2,29 @@
 
 namespace App\Services;
 
-use App\Services\CategoryService;
+use App\Repositories\CategoryRepository;
 
 use Yajra\Datatables\Datatables;
 
 class CategoryService extends Service {
 
-    public function __construct(CategoryService $category)
+    public function __construct(CategoryRepository $category)
     {
         $this->repo = $category;
     }
 
     public function getDatatables(): Object
     {
-        $datables = Datatables::of($this->getData())
-                    ->addIndexColumn()->addColumn('action', '
-                        <button class="btn btn-warning btn-sm" data-action="edit">
-                            <i class="fa fa-edit"></i>
-                        </button>
-                        <button class="btn btn-danger btn-sm" data-action="remove">
-                            <i class="fa fa-trash"></i>
-                        </button>
-                    ')->make();
-        return $datables;
-    }
+		$datatables = Datatables::of($this->getData())
+						->addIndexColumn()->addColumn('action', '
+                            <button class="btn btn-success btn-sm" data-action="edit">
+                                <i class="fa fa-edit"></i>
+                            </button>
+                            <button class="btn btn-danger btn-sm" data-action="remove">
+                                <i class="fa fa-trash"></i>
+                            </button>
+						')->make();
+		return $datatables;
+	}
 
 }
