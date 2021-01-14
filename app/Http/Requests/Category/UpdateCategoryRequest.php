4d37626e7ab1f3f908c57b180fa07642e0cd5873
dhanifu <mramdhani010@gmail.com>
2021-01-14 23:03:30 +0700

    Category Request

---
+++ app/Http/Requests/Category/UpdateCategoryRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required',
+            'name' => 'required|string|unique:categories,name,'.$this->id
+        ];
+    }
+}
