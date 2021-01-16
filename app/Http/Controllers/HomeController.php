da5197983acaebfb556cef21bcbd84179eb941eb
dhanifu <mramdhani010@gmail.com>
2021-01-16 17:50:43 +0700

    totalbarang dan total user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\StuffService;
+use App\Services\UserService;
+
 use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(StuffService $stuff, UserService $user)
     {
-        return view('home');
+        $totalStuff = $stuff->countData();
+        $totalUser = $user->countData();
+        return view('home', compact('totalStuff', 'totalUser'));
     }
 }
